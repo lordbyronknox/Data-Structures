e7fd579452e5ecb2557701e127ac82816cc6e46a
lordbyron <byronknox@yahoo.co.uk>
2022-04-21 11:57:47 +0200

    added shopping cart

--- exercises.js
+++ exercises.js
@@ -644,18 +644,20 @@ palindrome.     */
 
 
 
-
+/*
 3. Modify the priority queue example from Example 5-5 so that the higher-priority
 elements have higher numbers rather than lower numbers. Test your implementa‐
-tion with the example in the chapter.
+tion with the example in the chapter.   */
 
 
+
+/*
 4. Modify the ED example (Example 5-5) so the user can control the activity in the
 ED. Create a menu system that allows the user to choose from the following activ‐
 ities:
 a. Patient enters ED.
 b. Patient is seen by doctor.
-c. Display list of patients waiting to be seen. 
+c. Display list of patients waiting to be seen. */
 
 
 
